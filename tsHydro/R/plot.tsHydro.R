6fd5a9c97ce6bc360c5efa63dc1ac26b2f453bed
cavios <karni@space.dtu.dk>
2016-08-09 21:11:44 +0200

    corrected zoomout bug

--- tsHydro/R/plot.tsHydro.R
+++ tsHydro/R/plot.tsHydro.R
@@ -10,12 +10,12 @@ function(x,addRawDat=TRUE,addLine=TRUE,addError=FALSE,zoomOut=FALSE,lwd=4,col='b
             points(x$data$times[x$data$timeidx], x$data$height, pch=1, cex=1,col=gray(0.4))
             points(time,wl,...)
         }
-        if(addLine)lines(time,wl,lty=2,lwd=lwd,col=col,...) 
-        if(addError)arrows(time, wl-2*wlsd, time, wl+2*wlsd, length=0.05, angle=90, code=3,...)
         if(zoomOut){
              plot(x$data$times[x$data$timeidx], x$data$height, pch=1, cex=1,col=gray(0.4),xlab="time",ylab="Elevation")
              points(time,wl,...)
 
         }
-         }
+        if(addLine)lines(time,wl,lty=2,lwd=lwd,col=col,...) 
+        if(addError)arrows(time, wl-2*wlsd, time, wl+2*wlsd, length=0.05, angle=90, code=3,...)
+    }
 
